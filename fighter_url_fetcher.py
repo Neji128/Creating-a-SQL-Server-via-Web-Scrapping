c44aafbd792e061444ca8b59515fc79353c6c996
unknown <webbdesmondj@gmail.com>
2020-07-26 23:51:02 -0700

    fighter_url_scrapper converted to .py file for automation and modularization

---
+++ fighter_url_fetcher.py
@@ -0,0 +1,25 @@
+import pickle
+from bs4 import BeautifulSoup
+import requests, re
+import json
+
+fighter_url_list = []
+response = requests.get('http://www.ufcstats.com/statistics/fighters?char=a&page=all')
+soup = BeautifulSoup(response.content, 'html.parser')
+
+#targeting URLs that contain fighter data
+fighters = soup.findAll('a', attrs={'href': re.compile('http:')})
+for fighter in fighters:
+    fighter_url_list.append(fighter.get('href'))
+
+#removing duplicate URLs
+fighter_url_list = list(set(fighter_url_list))
+
+#removing invalid URLs
+for url in fighter_url_list:
+    if 'fighter-details' not in url:
+        fighter_url_list.remove(url)
+
+#saving output for later manipulation
+with open('fighter_url_list', 'wb') as f:
+    pickle.dump(fighter_url_list, f)
